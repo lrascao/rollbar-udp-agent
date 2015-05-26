465c71a5a654f806553cfafdd9c920d858c14ffe
Luis Rascao <luis.rascao@miniclip.com>
2015-05-26 11:04:20 +0100

    Prepare release 0.0.14

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError:
 
 setup(
     name='rollbar-udp-agent',
-    version='0.0.13',
+    version='0.0.14',
     description='Rollbar server-side UDP agent',
     long_description=README,
     author='Luis Rascão',
